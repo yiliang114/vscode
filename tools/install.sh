a1cc696a408c485a34022588acf4cf1aa5268472
yiliang114 <1204183885@qq.com>
2023-04-23 14:02:12 +0800

    add build tools

---
+++ tools/install.sh
@@ -0,0 +1,1 @@
+# yum install libsecret-devel libx11-devel.x86_64 libxkbfile-devel.x86_64 && PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1 && ELECTRON_SKIP_BINARY_DOWNLOAD=1 && yarn --frozen-lockfile --network-timeout 180000
