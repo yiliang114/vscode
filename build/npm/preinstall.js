9b7cb4e366977d222bd6da389f0b7444dab66354
yiliang114 <1204183885@qq.com>
2023-07-18 21:04:54 +0800

    feat: 注释装依赖之前对 node 大于 17 的校验

--- build/npm/preinstall.js
+++ build/npm/preinstall.js
@@ -13,9 +13,10 @@ if (majorNodeVersion < 16 || (majorNodeVersion === 16 && minorNodeVersion < 17))
 	console.error('\033[1;31m*** Please use node.js versions >=16.17.x and <17.\033[0;0m');
 	err = true;
 }
-if (majorNodeVersion >= 17) {
-	console.warn('\033[1;31m*** Warning: Versions of node.js >= 17 have not been tested.\033[0;0m')
-}
+// node@16 is about to be abandoned on vercel.
+// if (majorNodeVersion >= 17) {
+// 	console.warn('\033[1;31m*** Warning: Versions of node.js >= 17 have not been tested.\033[0;0m')
+// }
 
 const path = require('path');
 const fs = require('fs');
